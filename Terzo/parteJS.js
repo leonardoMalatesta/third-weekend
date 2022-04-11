74c7408d749673bb873e165ac63c0f44860d10de
Leonardo <leonardomalatesta18@gmail.com>
2022-04-11 07:42:29 +0200

    Aggiornamento "finale" esercizi

---
+++ Terzo/parteJS.js
@@ -0,0 +1,47 @@
+function checkLastName() {
+    if (document.registration.lastname.value == "") {
+        alert("Insert Last Name");
+        return false;
+    }
+    return true;
+}
+
+function checkName() {
+    if (document.registration.name.value == "") {
+        alert("Insert Name");
+        return false;
+    }
+    return true;
+}
+
+function checkID() {
+    if (isNaN(document.registration.matricola.value)) {
+        alert("Invalid Number");
+        return false;
+    }
+    return true;
+}
+
+function checkRegion() {
+    if (document.registration.region.value == "none") {
+        alert("Selezionare una regione");
+        return false;
+    }
+    return true;
+}
+
+function checkContact() {
+    if ((document.registration.email.value == "") && (document.registration.tel.value == "")) {
+        alert("Insert E-mail or Telephone number");
+        return false;
+    }
+    return true;
+}
+
+function checkAll() {
+    this.checkLastName();
+    this.checkName();
+    this.checkID();
+    checkRegion();
+    checkContact();
+}
